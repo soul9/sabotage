7eae8e93cc5a2ac0797b2965cf32256959433410
rofl0r <retnyg@gmx.net>
2012-04-20 18:19:18 +0200

    add some stuff to hd-writer

--- utils/write-hd-image.sh
+++ utils/write-hd-image.sh
@@ -7,10 +7,12 @@ echo_bold() {
 }
 
 usage() {
-	echo_bold "Usage: $0 <img file> <directory or tarball of content> <img size> [--clear-builds|--copy-tarballs]"
+	echo_bold "Usage: $0 <img file> <directory or tarball of content> <img size> [options]"
+	echo "options: --clear-builds --copy-tarballs --zero-fill"
 	echo
 	echo "--clear-builds will remove stuff in /src/build (butch 0.0.8+ build directory)"
 	echo '--copy-tarballs will copy tarballs from directory pointed to by "C" env var'
+	echo "--zero-fill will fill the rest of the hd image with zero for better compressability"
 	echo
 	echo "<img size> will be passed directly to dd, so you can use whatever value dd supports, i.e. 8G"
 	exit 1
@@ -40,9 +42,16 @@ check_opts() {
 				clear_builds=1;;
 			--copy-tarballs)
 				copy_tarballs=1;;
+			--zero-fill)
+				zero_fill=1;;
 		esac
 		shift
 	done
+}
+
+run_echo() {
+	printf "%s\n", "$@"
+	"$@"
 }
 
 mountdir=
@@ -69,7 +78,11 @@ for mbr_bin in mbr.bin /usr/lib/syslinux/mbr.bin /usr/share/syslinux/mbr.bin
 [ -z "$mbr_bin" ] && die 'Could not find mbr.bin'
 
 echo_bold "1) make the image file"
-dd if=/dev/zero of="$imagefile" bs=1 count=1 seek="$imagesize" || die "Failed to create $imagefile"
+if [ "$zero_fill = "1" ] ; then
+	time dd if=/dev/zero of="$imagefile" bs=1 count="$imagesize" || die "Failed to create $imagefile"
+else
+	dd if=/dev/zero of="$imagefile" bs=1 count=1 seek="$imagesize" || die "Failed to create $imagefile"
+fi
 
 echo_bold "2) fdisk"
 echo 'n
@@ -96,8 +109,11 @@ dd conv=notrunc if="$mbr_bin" of="$imagefile" || die 'Failed to set up MBR'
 echo_bold '4) /boot'
 loopdev=`losetup -f`
 mountdir="/tmp/mnt.$$"
-losetup -o $part1_start --sizelimit $part1_size "$loopdev" "$imagefile" || die 'Failed to losetup for /boot'
-mkdir -p "$mountdir" || die_unloop 'Failed to make '"$mountdir"
+
+echo_bold "info: mounting $imagefile as $loopdev on $mountdir"
+
+run_echo losetup -o $part1_start --sizelimit $part1_size "$loopdev" "$imagefile" || die 'Failed to losetup for /boot'
+mkdir -p "$mountdir" || die_unloop 'Failed to create '"$mountdir"
 mkfs.ext3 "$loopdev" || die_unloop 'Failed to mkfs.ext3 loop for /boot'
 mount "$loopdev" "$mountdir" || die_unloop 'Failed to mount loop for /boot'
 
@@ -116,17 +132,27 @@ losetup -d "$loopdev"
 
 echo_bold ' 5) /'
 loopdev=`losetup -f`
-losetup -o $part2_start "$loopdev" "$imagefile" || die 'Failed to losetup for /'
+run_echo losetup -o $part2_start "$loopdev" "$imagefile" || die 'Failed to losetup for /'
 mkfs.ext3 "$loopdev" || die_unloop 'Failed to mkfs.ext3 loop for /'
 mount "$loopdev" "$mountdir" || die_unloop 'Failed to mount loop for /'
 
 echo_bold "copying contents, this will take a while"
 if [ -d "$contents" ]
 then
-	[ "$clear_builds" = "1" ] && rm -rf "$mountdir/src/build/*"
-	[ "$copy_tarballs" = "1" ] && cp -f "$C/"* "$mountdir/src/tarballs/"
+	[ "$clear_builds" = "1" ] && rm -rf "$mountdir/src/build/"*
+	if [ "$copy_tarballs" = "1" ] ; then
+		mkdir -p "$mountdir/src/tarballs/"
+		cp -f "$C/"* "$mountdir/src/tarballs/"
+	fi
 
 	time cp -a "$contents"/* "$mountdir"/ || die_unmount 'Failed to copy /'
+	if [ "$zero_fill" = "1" ] ; then
+		echo_bold "filling rest of image with 0"
+		time dd if=/dev/zero of="$mountdir"/tmp/zero.img
+		rm -f "$mountdir"/tmp/zero.img
+	fi
+	ls_contents=`ls "$mountdir/"`
+	printf "%s\n" "$ls_contents"
 else
 	time tar -C "$mountdir" -zxf "$contents" || die_unmount 'Failed to extract /'
 fi
