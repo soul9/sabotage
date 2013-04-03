503c2da975878c6d47af52f3437a60af933c2dba
rofl0r <retnyg@gmx.net>
2013-04-03 02:03:21 +0200

    add convenience script for building dual kernel

---
+++ utils/build-kernel.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+if butch rebuild kernel ; then
+mv /boot/vmlinuz /boot/vmlinuz-grsec
+mv /boot/System.map /boot/System.map-grsec
+cat << EOF > /boot/extlinux.conf
+PROMPT 1
+TIMEOUT 100
+DEFAULT sabotage
+
+LABEL sabotage
+        KERNEL vmlinuz
+        APPEND root=/dev/sda1 rw vga=ask
+
+LABEL sabotage-grsec
+        KERNEL vmlinuz-grsec
+        APPEND root=/dev/sda1 rw vga=ask
+
+LABEL rescue
+        KERNEL vmlinuz
+        APPEND root=/dev/sda1 rw
+
+EOF
+DISABLE_GRSEC=1 butch rebuild kernel
+fi
