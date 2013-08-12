ca23b8f95ccf1c4756df8c4417e1078c0d53d220
rofl0r <retnyg@gmx.net>
2013-08-12 09:21:23 +0200

    bump butch to 0.3.0 and simplify build process
    it's no longer needed to source the config before building stage0,
    nor is it needed inside the rootfs before invoking butch.
    
    butch is now being installed as "butch-core" and butch is a wrapper
    script that sources the config.
    
    that makes it easy to migrate the config into /etc/butch.conf at
    some point.
    
    an additional change of butch is that the package db, previously
    /src/pkg/installed.dat, is now installed into /var/lib/butch.db
    instead. that way /src/pkg/ can be removed without deleting the
    package db by accident.
    a reason why one may want to do that is to symlink pkg and KEEP
    to a sabotage git checkout, so you automatically have fresh
    packages after a git pull.
    
    these changes require that $H is set to $PWD before the
    stage0 config is sourced (automatically done by build-stage),
    and that the config is aware of $H.
    unfortunately this requires existing checkouts to update their config,
    if they want to omit the ". config" step.

--- utils/resume-stage0.sh
+++ utils/resume-stage0.sh
@@ -6,4 +6,4 @@
 # this var is usually set by build-stage; omitting it results in build error of stage0_finish
 export H="$PWD"
 
-$S/butch.bin install stage0
+CONFIG="$H"/config "$R"/bin/butch install stage0
