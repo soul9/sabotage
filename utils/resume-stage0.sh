soul9/sabotage

commit ca23b8f95ccf1c4756df8c4417e1078c0d53d220

bump butch to 0.3.0 and simplify build process
it's no longer needed to source the config before building stage0,
nor is it needed inside the rootfs before invoking butch.

butch is now being installed as "butch-core" and butch is a wrapper
script that sources the config.

that makes it easy to migrate the config into /etc/butch.conf at
some point.

an additional change of butch is that the package db, previously
/src/pkg/installed.dat, is now installed into /var/lib/butch.db
instead. that way /src/pkg/ can be removed without deleting the
package db by accident.
a reason why one may want to do that is to symlink pkg and KEEP
to a sabotage git checkout, so you automatically have fresh
packages after a git pull.

these changes require that $H is set to $PWD before the
stage0 config is sourced (automatically done by build-stage),
and that the config is aware of $H.
unfortunately this requires existing checkouts to update their config,
if they want to omit the ". config" step.

utils/resume-stage0.sh
@@ -6,4 +6,4 @@
 # this var is usually set by build-stage; omitting it results in build error of stage0_finish
 export H="$PWD"
 
-$S/butch.bin install stage0
+CONFIG="$H"/config "$R"/bin/butch install stage0

KEEP/bin/butch
@@ -1,0 +1,14 @@
+#!/bin/sh
+[ "$STAGE" = "0" ] && [ -z "$CONFIG" ] && CONFIG="$PWD/config"
+[ -z "$CONFIG" ] && CONFIG=/src/config
+
+if [ ! -e "$CONFIG" ] ; then
+	echo "error: $CONFIG not found"
+	exit 1
+fi
+
+source "$CONFIG"
+
+BINDIR=$(dirname "$(readlink -f "$0")")
+exec "$BINDIR"/butch-core $*
+

KEEP/config.stage0
@@ -46,11 +46,15 @@ export ARM_FPU=vfp
 # S is the directory to extract tarballs and place sources for building stage0
 # R is the directory underwhich the new filesystem will be created
 # LOGPATH is the directory to place logfiles of builds
-export K="$PWD/KEEP"
-export C="$PWD/tarballs"
+
+# needed to inherit $PWD from toplevel buildscript
+[ -z "$H" ] && H="$PWD"
+
+export K="$H"/KEEP
+export C="$H"/tarballs
 export R="$SABOTAGE_BUILDDIR"
 export S="$R"/src
-export LOGPATH="$S/logs"
+export LOGPATH="$S"/logs
 
 # disable template which could mess up the build
 export BUTCH_BUILD_TEMPLATE="$K"/butch_template_configure_cached.txt

README
@@ -43,13 +43,10 @@ DO NOT RUN SCRIPTS YOU HAVE NOT READ.
   # set SABOTAGE_BUILDDIR, A, and MAKE_THREADS variables. 
   # the other values can be left as-is usually.
 
-% . ./config       # required!
-
 % ./build-stage 0  # build toolchain (~2min on an AMD FX 8core, 75min on ARM Cortex A8 800Mhz)
 % ./enter-chroot   # enter $R chrooted, needs root password
 % cd /src
 % vi config        # set your MAKE_THREADS, etc
-% . config
 % butch install stage1
 # and after that, feel free to install optional packages:
 # you can look at what's available using 'ls /src/pkg'
@@ -73,40 +70,18 @@ or you can simply copy the rootfs in $R to your medium of choice.
 
 The default root password is "sabotage".
 
-as usual, 
-
-% source /src/config
-
-so that butch has its environment variables set.
-
 you may want to generate some sshd-keys for sshd to use; use
 
 % butch install sshd-keys
 
 the sshd itself can be started using /sbin/sshd
 
-if you need dhcp, run
+check and edit /etc/rc.local for things you may want to autostart,
+such as network config, dhcp, linux console keymap...
 
-% butch install ndhcp-init
-
-this will create users and chroot for ndchp to use, afterwards you use
-
-% /etc/rc.dhcp start
-
-to run it.
-
-an alternate dhcp client is uclibc's udhcpc,
-which can be launched using /bin/dhclient.
-
-otherwise use ifconfig, route and /etc/resolve.conf to configure your static ip.
-you can of course put it into a script, and launch it automatically using
-/etc/rc.local
-
-for example
-% echo nameserver 8.8.8.8 > /etc/resolv.conf
-% ifconfig eth0 192.168.1.2 netmask 255.255.255.0
-% route add default gw 192.168.0.1
-
+if you have X installed, you may want to edit /bin/X for the correct evdev
+settings (see examples provided in there), then run "startx".
+also check /etc/xinitrc for X11 keyboard config.
 
 NOTE TO CONTRIBUTORS
 if you want to add packages, start from KEEP/package.template .

build-stage
@@ -1,6 +1,6 @@
 #!/bin/sh
-
 export H="$PWD"
+source ./config
 
 if [ ! -r "$H"/config ]; then
     printf -- "couldn't read configuration file: %s\n" "$H/config"
@@ -48,15 +48,15 @@ EOF
 # NOTE
 # installation starts all over if installed.dat is not there.
 # to manually continue a stage0 build, run
-#     $S/butch.bin install stage0
+#     $R/bin/butch install stage0
 # in case the build failed or was interrupted.
 rm -f "$S"/pkg/installed.dat
 
 if [ "$STAGE" = "0" ] ; then
 	# build stage0
-	"$S"/butch.bin install stage0
+	CONFIG="$H/config" BUTCHDB="$R"/var/lib/butch.db "$R"/bin/butch install stage0
 elif [ "$STAGE" = "u" ] ; then
-	mv "$S/butch.bin" "$R/bin/butch"
+	mv "$R/bin/butch" "$R/bin/butch"
 	path_save="$PATH"
 	export PATH="$R/bin/:$PATH"
 	export STAGEU=1

enter-chroot
@@ -1,4 +1,5 @@
 #!/bin/sh
+source ./config
 if [ -z "$R" ] ; then
 	echo 'no environment vars set, did you run . config first ?'
 	exit 1

pkg/butch
@@ -1,11 +1,11 @@
 [mirrors]
-http://codu.org/~rofl/butch-0.2.1.tar.bz2
-http://mirror.wzff.de/butch/butch-0.2.1.tar.bz2
-http://foss.aueb.gr/mirrors/linux/butch/butch-0.2.1.tar.bz2
+http://codu.org/~rofl/butch-0.3.0.tar.bz2
+http://mirror.wzff.de/butch/butch-0.3.0.tar.bz2
+http://foss.aueb.gr/mirrors/linux/butch/butch-0.3.0.tar.bz2
 
 [main]
-filesize=65001
-sha512=1be8ad50c1c2430213bacdba3958d35344c81b05c8aba1bc8805cd577161467bcf8515ae61e0663fab4009563b6d6b8464179fe023a7983e0f40a855cdb15ad8
+filesize=65176
+sha512=23f81cafdf3d81a4598560caaf8becbccdced79d09e033405f0434a7d4a823f8f19ba2501d8f14a8c41c53d461238a276e7771f0c7f274264949c964834ef0df
 tardir=butch
 
 [deps]
@@ -16,14 +16,11 @@ stage0_musl
 if [ ! -z "$BUTCH_FROM_GIT" ] ; then
 	rm -rf butch
 	git clone https://github.com/rofl0r/butch
-else
-	patch -p1 < "$K"/butch-021-memory_corruption.patch || exit 1
 fi
 
 # prepare build config...
-printf "LDFLAGS = -static\n" > butch/config.mak
-[ -z "$DEBUGBUILD" ] || printf "CFLAGS += -g3\n" >> butch/config.mak
-echo "CFLAGS += -O0" >> butch/config.mak
+printf "LDFLAGS += -static %s\n" "$optldflags" > butch/config.mak
+printf "CFLAGS += %s\n" "$optcflags" >> butch/config.mak
 
 if [ "$A" = "arm" ] || [ "$A" = "mips" ] ; then 
 	# if on arm or mips, we only use one build thread to not exhaust memory
@@ -37,10 +34,11 @@ fi
 [ "$STAGE" = "0" ] && CC="$butch_root_dir"/bin/musl-gcc
 CC="$CC -static" ./build.sh || exit 1
 BUTCH_BIN=butch/butch
+mv "$BUTCH_BIN" "$BUTCH_BIN"-core
 
 # "make install"
 dest=$butch_install_dir$butch_prefix
 mkdir -p "$dest"/bin
-cp -f "$BUTCH_BIN" "$dest"/bin/butch || exit 1
+cp -f "$BUTCH_BIN"-core "$dest"/bin/butch-core || exit 1
 
 

utils/build-butch
@@ -1,12 +1,14 @@
 #!/bin/sh
-filesize='65001'
-sha512='1be8ad50c1c2430213bacdba3958d35344c81b05c8aba1bc8805cd577161467bcf8515ae61e0663fab4009563b6d6b8464179fe023a7983e0f40a855cdb15ad8'
-version=0.2.1
+filesize='65176'
+sha512='23f81cafdf3d81a4598560caaf8becbccdced79d09e033405f0434a7d4a823f8f19ba2501d8f14a8c41c53d461238a276e7771f0c7f274264949c964834ef0df'
+version=0.3.0
 
 filename="$C"/butch-$version.tar.bz2
 
 if [ -z "$BUTCH_BIN" ] ; then
 
+tarxf http://codu.org/~rofl/ butch-$version .tar.bz2 butch ||
+tarxf http://mirror.wzff.de/butch/ butch-$version .tar.bz2 butch ||
 tarxf http://foss.aueb.gr/mirrors/linux/butch/ butch-$version .tar.bz2 butch
 
 getfilesize() {
@@ -47,5 +49,5 @@ BUTCH_BIN=butch/butch
 
 fi
 
-cp "$BUTCH_BIN" "$S"/butch.bin
+cp "$BUTCH_BIN" "$R"/bin/butch-core
 

commit 26b58c9858c10ce425279b61af1d668500c101c5

fix utils/resume-stage0
regression from ca23b8f95ccf1c4756df8c4417e1078c0d53d220

utils/resume-stage0.sh
@@ -6,4 +6,4 @@
 # this var is usually set by build-stage; omitting it results in build error of stage0_finish
 export H="$PWD"
 
-CONFIG="$H"/config "$R"/bin/butch install stage0
+CONFIG="$H"/config BUTCHDB="$R"/var/lib/butch.db "$R"/bin/butch install stage0