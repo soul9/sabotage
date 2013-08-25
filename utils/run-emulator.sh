a350a7f94b7c2e849ef021cbad02d59b0bda88f9
rofl0r <retnyg@gmx.net>
2013-08-25 23:59:00 +0200

    add util to boot chroot with kernel directly in qemu
    virtio over plan9 is used.
    tested on x86_64.

---
+++ utils/run-emulator.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+export H="$PWD"
+source ./config
+
+console=
+rw=rw
+for i ; do
+	[ "$i" = --console ] && console="console=ttyS0"
+	[ "$i" = --ro ] && rw=ro
+done
+nographic=
+[ -z "$console" ] || nographic="-nographic"
+[ -z "$INIT" ] && INIT=/bin/init
+[ -z "$KERNEL" ] && KERNEL=$R/boot/vmlinuz
+
+qemu-system-$A $nographic -no-reboot -kernel $KERNEL -append \
+  "root=root $rw rootflags=$rw,trans=virtio,version=9p2000.L rootfstype=9p init=$INIT panic=1 HOST=$A $console" \
+  -fsdev local,id=root,path=$R,security_model=none -device virtio-9p-pci,fsdev=root,mount_tag=root
