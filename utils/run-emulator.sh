9f642ea1c1b160b19908849504c0c59cd25d48c5
rofl0r <retnyg@gmx.net>
2013-09-29 02:57:03 +0200

    run-emulator: increase default ram to 140 MB
    using qemu's default of 128 MB, starting any new process on the X
    desktop fails with "fork: out of memory".
    since there is memory left, it must be due to some unlucky mappings
    right next to heap, so musl can't satisfy small allocations using
    sbrk.

--- utils/run-emulator.sh
+++ utils/run-emulator.sh
@@ -13,7 +13,8 @@ nographic=
 [ -z "$console" ] && console="vga=ask"
 [ -z "$INIT" ] && INIT=/bin/init
 [ -z "$KERNEL" ] && KERNEL=$R/boot/vmlinuz
+[ -z "$ram" ] && ram=140
 
-qemu-system-$A $nographic -no-reboot -kernel $KERNEL -append \
+qemu-system-$A $nographic -m $ram -no-reboot -kernel $KERNEL -append \
   "root=root $rw rootflags=$rw,trans=virtio,version=9p2000.L rootfstype=9p init=$INIT panic=1 HOST=$A $console" \
   -fsdev local,id=root,path=$R,security_model=none -device virtio-9p-pci,fsdev=root,mount_tag=root
