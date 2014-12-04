bc0167a20f517749486406dc3373999e47321c5f
rofl0r <retnyg@gmx.net>
2014-12-04 10:41:23 +0100

    wpa_connect_action: improve usability
    ship with ready-to-use options and flags to select between
    dhcp/non-dhcp setups and automatic connection throttling (necessary
    with some wifi drivers to achieve a stable connection).

--- KEEP/etc/wpa_connect_action.sh
+++ KEEP/etc/wpa_connect_action.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
+
+# set to false if you want a static ip
+# (and edit ip address settings further down)
+do_dhcp=true
+# set to true if you experience connection problems
+do_throttle=false
+
 if="$1"
 state="$2"
 echo "$0: $if $state"
 case "$state" in
 CONNECTED)
-dhclient "$if" || dhclient "$if";
+if $do_dhcp ; then
+	dhclient "$if" || dhclient "$if";
+else
+	sn=192.168.1
+	ifconfig "$if" "$sn".100 netmask 255.255.255.0
+	route delete default
+	route add default gw "$sn".1
+fi
+$do_throttle && iwconfig "$if" rate 1M
 ;;
 DISCONNECTED)
 :
