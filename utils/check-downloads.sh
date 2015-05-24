59a12923c38f11a0e94f4d735d4b9c8cd6bf0146
JillTyrell <jt@deadlytowers.org>
2015-05-24 03:15:22 +0000

    utils/check-downloads.sh: Updated to support BusyBox wget; added docs.

--- utils/check-downloads.sh
+++ utils/check-downloads.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-# use like: cat pkg/* | utils/extract-links.sh | utils/check-downloads.sh
+# use: cat pkg/* | utils/extract-links.sh | utils/check-downloads.sh
+# check-downloads.sh will echo links wget reports broken
+
+if (wget --help 2>&1 |grep -q BusyBox) ; then
+        CMD='wget -s -T 60 -q'
+else
+        CMD='wget --spider --timeout 60 -q'
+fi
+
 while read p ; do
-	wget --spider --timeout 60 -q "$p" || echo "$p"
+        $CMD "$p" 2>/dev/null || echo "$p"
 done
