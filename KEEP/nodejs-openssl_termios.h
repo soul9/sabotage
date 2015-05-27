3fefdc90d09f1c07c3ab767929c1d1dba9df0eb8
rofl0r <retnyg@gmx.net>
2015-05-27 12:00:37 +0100

    nodejs: bump to latest

--- KEEP/nodejs-openssl_termios.h
+++ KEEP/nodejs-openssl_termios.h
@@ -8,16 +8,3 @@
  #  if !defined(TERMIOS) && !defined(TERMIO) && !defined(SGTTY)
  #   define TERMIOS
  #  endif
-@@ -190,12 +191,6 @@
- # undef  SGTTY
- #endif
- 
--#if defined(linux) && !defined(TERMIO)
--# undef  TERMIOS
--# define TERMIO
--# undef  SGTTY
--#endif
--
- #ifdef _LIBC
- # undef  TERMIOS
- # define TERMIO
