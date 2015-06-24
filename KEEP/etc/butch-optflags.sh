67d219adb799543d90468933323f46cdc2d936cf
John Soros <johnny@r0x0r.me>
2015-06-24 21:20:53 +0200

    butch-optflags: clean up, add HARDENED option   HARDENED adds gcc stack-smashing protection to the cflags

--- KEEP/etc/butch-optflags.sh
+++ KEEP/etc/butch-optflags.sh
@@ -9,8 +9,25 @@ isgcc3() {
 	$mycc --version | grep "3.4.6" >/dev/null
 }
 
-optcflags="-fdata-sections -ffunction-sections -Os -g0 -fno-unwind-tables -fno-asynchronous-unwind-tables -Wa,--noexecstack"
+optcflags="-fdata-sections -ffunction-sections -Os -g0 -fno-unwind-tables -fno-asynchronous-unwind-tables -Wa,--noexecstack -pipe"
 optldflags="-s -Wl,--gc-sections -Wl,-z,relro,-z,now"
+
+if ! isgcc3; then
+	if [ "$HARDENED" = "1" ]; then
+		optcflags="$optcflags -fPIC -fstack-protector-all -D_FORTIFY_SOURCE=2"
+		case "$A" in
+			i?86) optldflags="-lssp_nonshared $optldflags";;
+		esac
+	fi
+
+	if [ "$BRUTE" = 2 ] ; then #more agressive optimizations
+		optcflags="$optcflags -O0 -flto=jobserver"
+		optldflags="$optldflags -O3 -flto=jobserver"
+	elif [ "$BRUTE" = 1 ] ; then #if you get "recompile with -fPIC" errors it's probably due to this
+		optcflags="$optcflags -O0 -flto=jobserver"
+		optldflags="$optldflags -Os -flto=jobserver"
+	fi
+fi
 
 if [ "$DEBUGBUILD" = "1" ] ; then
 	# use "DEBUGBUILD=1 butch install mypkg" to create debug version of mypkg
@@ -19,14 +36,4 @@ if [ "$DEBUGBUILD" = "1" ] ; then
 elif [ "$TESTBUILD" = "1" ] ; then
 	optcflags="-O0 -g0"
 	optldflags=
-else
-	[ "$STAGE" = "0" ] || isgcc3 || optcflags="$optcflags -ftree-dce"
-fi
-
-if [ "$BRUTE" = 2 ] ; then
-	optcflags="$optcflags -s -Os -flto -fwhole-program"
-	optldflags="$optldflags -flto -fwhole-program"
-elif [ "$BRUTE" = 1 ] ; then
-        optcflags="$optcflags -s -Os -flto"
-        optldflags="$optldflags -flto"
 fi
